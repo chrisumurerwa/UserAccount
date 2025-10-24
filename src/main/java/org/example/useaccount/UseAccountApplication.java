94e1216e477f017743ad58db437cf9551db90d4c
chrisumurerwa <chrisumurerwa1@gmail.com>
2025-10-24 09:48:19 +0200

    connecting with jpa

---
+++ src/main/java/org/example/useaccount/UseAccountApplication.java
@@ -0,0 +1,13 @@
+package org.example.useaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UseAccountApplication.class, args);
+    }
+
+}
